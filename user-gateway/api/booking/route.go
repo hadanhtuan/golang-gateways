5081030e5a6dc99745d2f39a3dd0fd47c90f7bb5
Trong Tin <trantrongtin01012002@gmail.com>
2024-03-11 00:22:52 +0700

    feat review done

--- user-gateway/api/booking/route.go
+++ user-gateway/api/booking/route.go
@@ -10,12 +10,18 @@ func InitRoute(router *gin.RouterGroup, app *pkg.App) error {
 	bookingController := app.Handler[app.Config.GRPC.BookingServicePort].(*BookingController)
 
 	bookingGroup := router.Group("/booking")
-
+	// Property
 	bookingGroup.GET("/:bookingId", bookingController.GetBookingDetail)
 	bookingGroup.GET("/property/:propertyId", bookingController.GetPropertyDetail)
 	bookingGroup.GET("/property", bookingController.GetAllProperty)
 	bookingGroup.POST("/property", bookingController.CreateProperty)
 	bookingGroup.PATCH("/property/:propertyId", bookingController.UpdateProperty)
 	bookingGroup.DELETE("/property/:propertyId", bookingController.DeleteProperty)
+	//Review
+	bookingGroup.POST("/review", bookingController.CreateReview)
+	bookingGroup.POST("/reviewGET", bookingController.GetReview)
+	bookingGroup.PATCH("/review/:reviewId", bookingController.UpdateReview)
+	bookingGroup.DELETE("/review/:reviewId", bookingController.DeleteReview)
+
 	return nil
 }
