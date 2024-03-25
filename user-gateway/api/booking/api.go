643fb3c4d89ac8bc2dfc091dbdb34648ece5a0e7
hadanhtuan <hadanhtuan1210@gmail.com>
2024-03-25 14:54:15 +0700

    feat: search

--- user-gateway/api/booking/api.go
+++ user-gateway/api/booking/api.go
@@ -2,7 +2,6 @@ package apiBooking
 
 import (
 	"context"
-	"fmt"
 	"time"
 	"user-gateway/internal/util"
 	bookingProto "user-gateway/proto/booking"
@@ -25,18 +24,21 @@ func (bc *BookingController) GetProperty(c *gin.Context) {
 	defer cancel()
 
 	var payload bookingProto.MsgQueryProperty
-	payload.Paginate = &sdk.Pagination{
-		Offset: 0,
-		Limit:  10,
-	}
 	err := c.BindJSON(&payload)
 
 	if err != nil {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
-			Message: "Invalid request",
+			Message: "Error parsing body. Error detail " + err.Error(),
 		})
 		return
+	}
+
+	if payload.Paginate == nil {
+		payload.Paginate = &sdk.Pagination{
+			Offset: 0,
+			Limit:  10,
+		}
 	}
 
 	result, _ := bc.ServiceBookingClient.GetProperty(ctx, &payload)
@@ -53,9 +55,9 @@ func (bc *BookingController) CreateProperty(c *gin.Context) {
 	err := c.BindJSON(&payload)
 
 	if err != nil {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
-			Message: "Invalid request",
+			Message: "Error parsing body. Error detail " + err.Error(),
 		})
 		return
 	}
@@ -72,9 +74,9 @@ func (bc *BookingController) UpdateProperty(c *gin.Context) {
 	err := c.BindJSON(&payload)
 
 	if err != nil {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
-			Message: "Invalid request",
+			Message: "Error parsing body. Error detail " + err.Error(),
 		})
 		return
 	}
@@ -90,7 +92,7 @@ func (bc *BookingController) DeleteProperty(c *gin.Context) {
 	var payload bookingProto.MsgDeleteProperty
 	propertyId := c.Param("propertyId")
 	if propertyId == "" {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
 			Message: "Invalid request",
 		})
@@ -111,7 +113,7 @@ func (bc *BookingController) GetBookingDetail(c *gin.Context) {
 	result, _ := bc.ServiceBookingClient.GetBookingDetail(ctx, &bookingProto.MsgGetBooking{
 		BookingId: id,
 	})
-	fmt.Println(result)
+
 	newResult := util.ConvertResult(result)
 	c.JSON(int(newResult.Status), newResult)
 }
@@ -123,9 +125,9 @@ func (bc *BookingController) CreateReview(c *gin.Context) {
 	err := c.BindJSON(&payload)
 
 	if err != nil {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
-			Message: "Invalid request",
+			Message: "Error parsing body. Error detail " + err.Error(),
 		})
 		return
 	}
@@ -140,9 +142,9 @@ func (bc *BookingController) UpdateReview(c *gin.Context) {
 	var payload bookingProto.MsgUpdateReview
 	err := c.BindJSON(&payload)
 	if err != nil {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
-			Message: "Invalid request",
+			Message: "Error parsing body. Error detail " + err.Error(),
 		})
 		return
 	}
@@ -158,7 +160,7 @@ func (bc *BookingController) DeleteReview(c *gin.Context) {
 	var payload bookingProto.MsgDeleteReview
 	reviewId := c.Param("reviewId")
 	if reviewId == "" {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
 			Message: "Invalid request",
 		})
@@ -175,6 +177,88 @@ func (bc *BookingController) GetReview(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 	var payload bookingProto.MessageQueryReview
+	err := c.BindJSON(&payload)
+
+	if err != nil {
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
+			Status:  common.APIStatus.BadRequest,
+			Message: "Error parsing body. Error detail " + err.Error(),
+		})
+		return
+	}
+
+	if payload.Paginate == nil {
+		payload.Paginate = &sdk.Pagination{
+			Offset: 0,
+			Limit:  10,
+		}
+	}
+
+	result, _ := bc.ServiceBookingClient.GetReview(ctx, &payload)
+	newResult := util.ConvertResult(result)
+	c.JSON(int(newResult.Status), newResult)
+}
+
+func (bc *BookingController) CreateAmenity(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	var payload bookingProto.MsgAmenity
+	err := c.BindJSON(&payload)
+
+	if err != nil {
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
+			Status:  common.APIStatus.BadRequest,
+			Message: "Error parsing body. Error detail " + err.Error(),
+		})
+		return
+	}
+	result, _ := bc.ServiceBookingClient.CreateAmenity(ctx, &payload)
+	newResult := util.ConvertResult(result)
+	c.JSON(int(newResult.Status), newResult)
+}
+
+func (bc *BookingController) UpdateAmenity(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	var payload bookingProto.MsgAmenity
+	err := c.BindJSON(&payload)
+	if err != nil {
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
+			Status:  common.APIStatus.BadRequest,
+			Message: "Error parsing body. Error detail " + err.Error(),
+		})
+		return
+	}
+	result, _ := bc.ServiceBookingClient.UpdateAmenity(ctx, &payload)
+	newResult := util.ConvertResult(result)
+
+	c.JSON(int(newResult.Status), newResult)
+}
+
+func (bc *BookingController) DeleteAmenity(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	var payload bookingProto.MsgId
+	amenityId := c.Param("amenityId")
+
+	if amenityId == "" {
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
+			Status:  common.APIStatus.BadRequest,
+			Message: "Invalid request",
+		})
+		return
+	}
+	payload.Id = amenityId
+	result, _ := bc.ServiceBookingClient.DeleteAmenity(ctx, &payload)
+	newResult := util.ConvertResult(result)
+
+	c.JSON(int(newResult.Status), newResult)
+}
+
+func (bc *BookingController) GetAmenity(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	var payload bookingProto.MessageQueryAmenity
 	payload.Paginate = &sdk.Pagination{
 		Offset: 0,
 		Limit:  10,
@@ -182,14 +266,14 @@ func (bc *BookingController) GetReview(c *gin.Context) {
 	err := c.BindJSON(&payload)
 
 	if err != nil {
-		c.JSON(int(common.APIStatus.BadRequest), &sdk.BaseResponse{
+		c.JSON(int(common.APIStatus.BadRequest), &common.APIResponse{
 			Status:  common.APIStatus.BadRequest,
-			Message: "Invalid request",
+			Message: "Error parsing body. Error detail " + err.Error(),
 		})
 		return
 	}
 
-	result, _ := bc.ServiceBookingClient.GetReview(ctx, &payload)
+	result, _ := bc.ServiceBookingClient.GetAmenity(ctx, &payload)
 	newResult := util.ConvertResult(result)
 	c.JSON(int(newResult.Status), newResult)
 }
