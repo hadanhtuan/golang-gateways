5eaabcec5f892828fc158299588b05833860f3a1
TuanSanbul <tuan.hd@sanbul.net>
2024-04-01 11:53:02 +0700

    feat: payment

--- user-gateway/api/user/route.go
+++ user-gateway/api/user/route.go
@@ -13,6 +13,7 @@ func InitRoute(router *gin.RouterGroup, app *sdk.App) error {
 	userGroup.POST("/login", userController.Login)
 	userGroup.POST("/get", userController.GetUsers)
 	userGroup.POST("/register", userController.Register)
+	userGroup.PATCH("", userController.UpdateUser)
 	userGroup.POST("/refresh-token", userController.RefreshToken)
 	userGroup.GET("/logout", userController.AuthorizeRequest(), userController.Logout)
 	userGroup.GET("/profile", userController.AuthorizeRequest(), userController.GetProfile)
